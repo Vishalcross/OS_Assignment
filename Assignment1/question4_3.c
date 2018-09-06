04f04189eec58d912151ae6ac628aa7ec1dbd19f
Tanmay-Kulkarni101 <tanmaygk97@gmail.com>
2018-09-06 23:55:50 +0530

    Final form
    Its done b****

--- Assignment1/question4_3.c
+++ Assignment1/question4_3.c
@@ -7,25 +7,15 @@ Preamble:
 The objective of this program is to create an application that is similar to a process 
 manager. It takes the number of times it show processes and how many processes at a time.
 Furhtermore, it gives the option of killing processes bases on user demand.
-WARNING: This program makes use of SIGKILL thus, it does not partake in clean  up. In general,
-it is recommended to use SIGTERM.
-NOTE: This program makes use of signals to communicate between the parent and the child. So, that 
-they wake up each other with the help of a SIGHUP.
 ********************************************************************************************/
 #include<stdio.h>
-#include<unistd.h>
-#include<signal.h>
-#include<stdlib.h>
-#include<sys/signal.h>
-#include<sys/wait.h>
 #include<string.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<errno.h>
 
-void handler1(int sigNum){
-	system("echo Parent Interrupted successfully");
-}
-void handler2(int sigNum){
-	system("echo Child Interrupted successfully");
-}
 int getValue(char* c){
     int length=strlen(c);
     int val=0;
@@ -41,6 +31,7 @@ int getValue(char* c){
     }
     return val;
 }
+
 void forkIt(int *val){
 	int fd[2];
 	pipe(fd);
@@ -76,8 +67,8 @@ void forkIt(int *val){
 		wait(&status);
 	}
 }
+
 int main(int argc,char* argv[]){
-	
 	if(argc!=4){
 		system("echo We dont do that here.");
 		system("echo USEAGE: timeDelay sleep number_of_processes how_many_times");
@@ -88,44 +79,67 @@ int main(int argc,char* argv[]){
     for(int i=1;i<argc;i++){
         val[i-1] = getValue(argv[i]);
     }
-	int fd[2];
-	signal(SIGHUP,handler1);
-	pipe(fd);
 	
-	while(1){
-		int pid=fork();
-		if(pid==0){
-			signal(SIGHUP,handler2);
-			for(int i=0;i<val[1];i++){
+	int fd1[2];
+	int fd2[2];
+	pipe(fd1);
+	pipe(fd2);
+	
+	int pid=fork();
+	if(pid==0){
+		while(1){
+			for(int i=0;i<val[2];i++){
 				forkIt(val);
 				sleep(val[0]);
 			}
-			kill(getppid(),SIGHUP);
-			sleep(1000);
-			int kid;
-			close(fd[1]);
-			read(fd[0],&kid,sizeof(kid));
-			printf("The kill id:%d\n",kid);
-			if(kid>0){
-				int id=0;
-				id=kill(kid,SIGKILL);
-				if(id<0){
-					printf("Termination failed\n");
+			close(fd1[0]);
+			int success=200;
+			write(fd1[1],&success,sizeof(int));
+			int val=-2123;
+			close(fd2[1]);
+			read(fd2[0],&val,sizeof(int));
+			if(val>0){
+				int id;
+				id=kill(val,SIGTERM);
+				if(id>=0){
+					printf("Termination Successful\n");
 				}
 				else{
-					printf("Successfull\n");
+					printf("Termination failed\n");
 				}
 			}
-			return 0;
+			else if(val==-2123){
+				printf("Value did not change\n");
+			}
 		}
-		else{
-			sleep(1000);
-			printf("Enter the process to be killed\n");
-			int temp;
-			scanf("%d",&temp);
-			close(fd[0]);
-			write(fd[1],&temp,sizeof(temp));
-			kill(pid,SIGHUP);
+	}
+	else{
+		while(1){
+			int val=-1;
+			close(fd1[1]);
+			int readVal;
+			readVal=read(fd1[0],&val,sizeof(int));
+			printf("read returned:%d\n",readVal);
+			if(val!=-1){
+				int kid;
+				printf("Enter the pid of the process to be killed\n");
+				scanf("%d",&kid);
+				close(fd2[0]);
+				write(fd2[1],&kid,sizeof(int));
+			}
+			else if(readVal==-1){
+				switch(errno){
+					case EBADF:printf("fd is not a valid file descriptor or is not open for reading.\n");break;
+					case EFAULT: printf("buf is outside your accessible address space.\n");break;
+					case EINTR:  printf("The call was interrupted by a signal before any data was read; see signal(7).\n");break;
+					case EINVAL: printf("fd is attached to an object which is unsuitable for reading; or the file was opened with the O_DIRECT flag, and either the address specified in  buf,\n");
+								 printf("the value specified in count, or the current file offset is not suitably aligned\n");
+								 printf("fd was created via a call to timerfd_create(2) and the wrong size buffer was given to read(); see timerfd_create(2) for further information.\n");break;
+					case EIO: 	 printf("I/O  error.   This will happen for example when the process is in a background process group, tries to read from its controlling terminal, and either\n");
+								 printf("it is ignoring or blocking SIGTTIN or its process group is orphaned.  It may also occur when there is a low-level I/O error while reading from a disk\n");
+								 printf("or tape.\n");break;
+				}
+			}
 		}
-	} 
+	}
 }
